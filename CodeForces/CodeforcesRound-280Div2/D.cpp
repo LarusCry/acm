5a679d658351b7beab77f5ef8f9f9987f24cf845
Larus <laruscry@gmail.com>
2014-12-03 22:43:43 +0800

    Create D.cpp

---
+++ CodeForces/CodeforcesRound-280Div2/D.cpp
@@ -0,0 +1,83 @@
+/*****************************************************
+  > File Name: D.cpp
+  > Author: Larus
+  > Mail: LarusCry@gmail.com
+  > Created Time: 2014年12月02日 星期二 09时08分32秒
+*****************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <functional>
+#include <bitset>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+#include <cctype>
+#include <cstdlib>
+#include <cassert>
+#include <ctime>
+#include <utility>
+#include <limits>
+#include <numeric>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+#define Max(a, b) ((FASTBUFFER = ((a) - (b)) >> 31), ((b) & FASTBUFFER | (a) & ~FASTBUFFER))
+#define Min(a, b) ((FASTBUFFER = ((a) - (b)) >> 31), ((a) & FASTBUFFER | (b) & ~FASTBUFFER))
+#define Swap(a, b) (a ^= b, b ^= a, a ^= b)
+#define OO 2147483647
+#define PQ priority_queue
+
+inline int readInt()
+{
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') { if (ch == '-') f = -1; ch = getchar(); }
+    while (ch >= '0' && ch <= '9') { x = x * 10 + ch - '0'; ch = getchar(); }
+    return x * f;
+}
+
+int FASTBUFFER;
+int n, x, y;
+
+void work()
+{
+    for (int i = 0; i < n; ++i) {
+        int t = readInt();
+        long long l = 0, r = 1e15, ans;
+        while (l <= r) {
+            long long mid = (l + r) >> 1;
+            if (mid / x + mid / y >= t) {
+                ans = mid;
+                r = mid - 1;
+            }
+            else
+                l = mid + 1;
+        }
+        if (ans % x == 0 && ans % y == 0)
+            printf("Both\n");
+        else if (ans % x)
+            printf("Vanya\n");
+        else
+            printf("Vova\n");
+    }
+}
+
+int main()
+{
+    //ios::sync_with_stdio(false);
+    while (~scanf("%d %d %d", &n, &x, &y)) {
+        work();
+    }
+    return 0;
+}
